c27ad117f26d847b00df5d25db942d1d4f12f52a
Gopisakthivel <gopisakthivel@alphabsolutions.com>
2024-04-07 19:20:17 +0530

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,15 @@
+const sectionHero = document.querySelector('.section-hero');
+const img = document.querySelectorAll('.design-card-img');
+
+let value = 250;
+
+// sectionHero.addEventListener('mousemove',(e)=> {
+//     img.forEach((item)=>{
+//         const data = item.dataset.value;
+//         const X = (e.clientX * data ) / value;
+//         const Y = (e.clientY * data) / value;
+//         item.style.setProperty("--mouse-x",`${X}px`)
+//         item.style.setProperty("--mouse-y",`${Y}px`)
+//     })
+// }) 
+
